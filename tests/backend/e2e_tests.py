3eb375c266fc1f818af2a4f3f82b197040d7319f
Gabriele Scorpaniti <gabriele.scorpaniti@outlook.com>
2025-09-03 17:16:46 +0200

    Fixed E2E test

--- tests/backend/e2e_tests.py
+++ tests/backend/e2e_tests.py
@@ -3,21 +3,16 @@ import requests
 import json
 
 
-def test_analyze_method(url):
+def test_analyze_method(url="http://localhost:8000/analyze"):
 
     with open("query.json", "r") as f:
         data = json.load(f)
 
     assert data is not None, "File query.json not loaded."
 
-    response = requests.post(url, json=data).json()
-
-    assert (
-        response.status_code == 200
-    ), f"Analyze method failed, got response {response.status}"
-
-    print(
-        f"Your check for {data['subject']}:\n"
-        f"Trust Score: {response['trust_score']}\n"
-        f"Details: {response['details']['comment']}"
+    resp = requests.post(url, json=data)
+    assert resp.status_code == 200, (
+        f"Analyze method failed, got response {resp.status_code}"
     )
+
+    print(resp.json())
