5c3cbacfd7b03d2b9235e530cfe5d06bde5d174f
Gabriele Scorpaniti <gabriele.scorpaniti@outlook.com>
2025-10-18 19:22:42 +0200

    Optimized for AzAS

--- frontend/app/App.jsx
+++ frontend/app/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../styles.css";
 
 const BACKEND_PORT = import.meta.env.VITE_BACKEND_PORT || window.BACKEND_PORT || 8000;
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || window.BACKEND_URL || `http://localhost:${BACKEND_PORT}`;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || window.BACKEND_URL || `https://trustme-backend.azurewebsites.net`;
 import { __VERSION__ } from "./config";
 
 export default function App({ user }) {
--- frontend/app/config.js
+++ frontend/app/config.js
@@ -1,1 +1,1 @@
-export const __VERSION__ = "0.1.1";
+export const __VERSION__ = "0.1.2";
